f606b25be062c106b7a1d1744b946f6dcbbc204f
FilOng <748957948@qq.com>
2018-04-13 20:23:04 +0800

    image.vo

---
+++ ImageSearch/src/com/feilong/spider/image/vo/Image.java
@@ -0,0 +1,39 @@
+package com.feilong.spider.image.vo;
+
+public class Image {
+	private int id;
+	private String src;
+	private String name;
+	private String path;
+	private int uid;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSrc() {
+		return src;
+	}
+	public void setSrc(String src) {
+		this.src = src;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+}
